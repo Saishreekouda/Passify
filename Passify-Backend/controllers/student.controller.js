710d5a5764bbbcd1033c77887eddc9819ba62868
Priyadarshini <priyadarshini.roy03@gmail.com>
2024-04-09 01:00:14 +0530

    getStudent controller

--- Passify-Backend/controllers/student.controller.js
+++ Passify-Backend/controllers/student.controller.js
@@ -3,8 +3,9 @@ import Student from "../models/student.model.js";
 export const getStudent = async (req, res) => {
   try {
     const { username } = req.body;
-    const student = await Student.find({ username: username });
-    res.status(200).json(student);
+
+    const student = await Student.find({ rollNumber: username });
+    res.status(200).json({ data: student });
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
