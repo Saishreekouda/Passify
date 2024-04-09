3ed3de46d366f09ae8f6f26d7284641b24c7b4a8
Ananya <goyalananya444@gmail.com>
2024-04-09 14:54:02 +0530

    profile-page-completed

--- Passify-Frontend/components/Profile.js
+++ Passify-Frontend/components/Profile.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
+import Navbar from '../components/Navbar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const ProfileScreen = () => {
   return (
+    <SafeAreaProvider>
     <View style={styles.container}>
       <View style={styles.avatarContainer}>
         <Image
@@ -12,18 +15,36 @@ const ProfileScreen = () => {
         <Text style={styles.name}>Jane Doe</Text>
       </View>
       <View style={styles.infoContainer}>
-        <Text style={styles.infoLabel}>Email:</Text>
-        <Text style={styles.infoValue}>jane.doe@example.com</Text>
+        <Text style={styles.infoLabel}>Roll Number:</Text>
+        <Text style={styles.infoValue}>IIT2021061</Text>
       </View>
       <View style={styles.infoContainer}>
-        <Text style={styles.infoLabel}>Location:</Text>
-        <Text style={styles.infoValue}>San Francisco, CA</Text>
+        <Text style={styles.infoLabel}>Semester:</Text>
+        <Text style={styles.infoValue}>3rd</Text>
       </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoLabel}>Program:</Text>
+        <Text style={styles.infoValue}>Information Technology</Text>
+      </View>
+      <View style={styles.infoContainer}>
+        <Text style={styles.infoLabel}>Phone:</Text>
+        <Text style={styles.infoValue}>9417927979</Text>
+      </View>
+      {/* Bio */}
       <View style={styles.infoContainer}>
         <Text style={styles.infoLabel}>Bio:</Text>
-        <Text style={styles.infoValue}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.</Text>
+        <Text style={styles.infoValue}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ornare magna eros, eu pellentesque tortor vestibulum ut. Maecenas non massa sem. Etiam finibus odio quis feugiat facilisis.
+        </Text>
       </View>
+      <View style={styles.navbar}>
+      <Navbar/>
+      </View>
+      
+     
+      
     </View>
+    </SafeAreaProvider>
   );
 };
 
@@ -32,6 +53,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     padding: 20,
+    paddingBottom:0
   },
   avatarContainer: {
     alignItems: 'center',
@@ -56,7 +78,12 @@ const styles = StyleSheet.create({
   infoValue: {
     marginTop: 5,
   },
+    navbar: {
+        flex:1,
+        marginLeft:-20,
+        marginRight:-20
+    },
+ 
 });
 
 export default ProfileScreen;
-                                 
